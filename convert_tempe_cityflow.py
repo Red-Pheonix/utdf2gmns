772f6dcc05674e918d026a9257cf6b7c1354d8e0
Red-Pheonix <rafirafi155@gmail.com>
2025-08-10 19:46:45 +0600

    added minor comments

--- convert_tempe_cityflow.py
+++ convert_tempe_cityflow.py
@@ -8,5 +8,7 @@ if __name__ == "__main__":
 
     # Step 1: Initialize the UTDF2GMNS
     net = ug.UTDF2GMNS(utdf_filename=path_utdf, region_name=region_name, verbose=False)
+    # Step 2: Geocode intersection, this is needed
     net.geocode_utdf_intersections(single_intersection_coord={}, dist_threshold=0.01)
+    # Step 3: convert UTDF network to Cityflow format
     net.utdf_to_cityflow(sim_name="tempe", sim_start_time=0, sim_duration=7200)
